f6bf54438b2f956fab40bee94602f1156c6f4c00
lgcholoquinga <luisg.choloquingal@gamil.com>
2023-10-09 19:19:02 -0500

    CRUD product

---
+++ src/products/products.service.ts
@@ -0,0 +1,76 @@
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Product } from './entities/product.entity';
+import { Repository } from 'typeorm';
+import { PaginationDto } from 'src/common/dtos/pagination.dto';
+
+@Injectable()
+export class ProductsService {
+  private logger = new Logger('ProductsService');
+
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+  ) {}
+
+  async create(createProductDto: CreateProductDto) {
+    try {
+      const product = this.productRepository.create(createProductDto);
+      await this.productRepository.save(product);
+      return product;
+    } catch (error) {
+      this.handleBDException(error);
+    }
+  }
+
+  async findAll(paginationDto: PaginationDto) {
+    const { limit = 10, offset = 0 } = paginationDto;
+    return await this.productRepository.find({ take: limit, skip: offset });
+  }
+
+  async findOne(id: string) {
+    const product = await this.productRepository.findOneBy({ id });
+
+    if (!product) {
+      throw new NotFoundException(`Product with id: ${id} not found.`);
+    }
+
+    return product;
+  }
+
+  update(id: number, updateProductDto: UpdateProductDto) {
+    return `This action updates a #${id} product`;
+  }
+
+  async remove(id: string) {
+    const product = await this.findOne(id);
+
+    if (!product) {
+      throw new NotFoundException(`Product with id: ${id} not found.`);
+    }
+
+    await this.productRepository.remove(product);
+    return `Product with id: ${id} deleted.`;
+  }
+
+  //#region Private methods
+  private handleBDException(error: any) {
+    if (error.code === '23505') {
+      throw new BadRequestException(error.detail);
+    }
+
+    this.logger.error(error);
+    throw new InternalServerErrorException(
+      'Unexpected error, check server logs.',
+    );
+  }
+  //#endregion
+}
